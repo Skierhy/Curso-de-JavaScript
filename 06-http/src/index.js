7475a0bd7136edf8375921c187bd75f523c4a4e4
Skierhy <Skierhy@gmail.com>
2022-09-13 22:29:59 -0500

    terminado el curso de javascript moderno

--- 06-http/src/index.js
+++ 06-http/src/index.js
@@ -1,39 +1,6 @@
-// import { obtenerUsuario } from './js/http-provider';
-import * as CRUD from './js/crud-provider';
-// import { init } from './js/usuarios-page';
 import './styles.css';
-// import { init } from './js/chistes-page';
+import './styles.css';
 
-// init();
+import { init } from './js/archivo-page';
 
-// obtenerUsuario().then(console.log);
-
-// init();
-CRUD.getUsuario(2).then((respuesta) => {
-	console.log('================GET================');
-	console.log(respuesta);
-});
-
-CRUD.createUsuario({
-	name: 'morpheus',
-	job: 'leader',
-}).then((respuesta) => {
-	console.log('=================POST===============');
-	console.log(respuesta);
-});
-
-CRUD.updateUsuario(
-	{
-		name: 'skierhy',
-		job: 'front-end developer',
-	},
-	2
-).then((respuesta) => {
-	console.log('=================PUT===============');
-	console.log(respuesta);
-});
-
-CRUD.deleteUsuario(2).then((respuesta) => {
-	console.log('=================DELETE===============');
-	console.log(respuesta);
-});
+init();
--- 06-http/src/js/http-provider.js
+++ 06-http/src/js/http-provider.js
@@ -1,6 +1,10 @@
 const jokeURL = 'https://api.chucknorris.io/jokes/random';
 const urlUsuario = 'https://reqres.in/api/users?page=2';
 
+// cludinary.com
+const cloudPreset = 'gkkbrld6';
+const cloudURL = 'https://api.cloudinary.com/v1_1/skierhy/upload';
+
 const obtenerChiste = async () => {
 	try {
 		const respuesta = await fetch(jokeURL);
@@ -27,4 +31,32 @@ const obtenerUsuario = async () => {
 	}
 };
 
-export { obtenerChiste, obtenerUsuario };
+// archivosubir :: File
+const subirImagen = async (archivoSubir) => {
+	// FormData :: Objeto que permite enviar archivos
+	// crea un formulario con los datos que le pasemos
+	const formData = new FormData();
+	/* Agregar un par clave-valor al objeto formData. */
+	// upload_preset :: clave
+	formData.append('upload_preset', cloudPreset);
+	// file es el nombre que le damos al archivo
+	formData.append('file', archivoSubir);
+	// puede fallar la subida de la imagen
+	try {
+		const respuesta = await fetch(cloudURL, {
+			method: 'POST',
+			body: formData,
+		});
+		if (respuesta.ok) {
+			const cloudResp = await respuesta.json();
+			// console.log(cloudResp);
+			return cloudResp.secure_url;
+		} else {
+			throw await respuesta.json();
+		}
+	} catch (error) {
+		throw error;
+	}
+};
+
+export { obtenerChiste, obtenerUsuario, subirImagen };
